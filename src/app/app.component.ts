54b0402439b21dfff9bf387375041caf609e757a
CaioOliveiraG2 <101058407+CaioOliveiraG2@users.noreply.github.com>
2022-11-15 17:52:24 -0300

    6 files modified, 2 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component, VERSION } from '@angular/core';
+import { BitcoinService } from './bitcoin.service';
+import { TimerService } from './timer.service';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  constructor(
+    public BitcoinService: BitcoinService,
+    public timer: TimerService
+  ) {
+    this.timer.start();
+  }
+
+  ngOnInit() {}
 }
